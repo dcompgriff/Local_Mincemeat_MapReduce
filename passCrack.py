37d68487f19aaeb2d24cc05be83a6af544e532e0
Dan Griffin <unknown7979@gmail.com>
2016-03-25 10:44:01 -0400

    Implemented dfs password permutation code for generating all permutations up to a max size. The dfs code takes a root string, and generates the powerset of the root string and all lowercase letters and numeric digits. By including the ability to set a root char, the generation can be split into many different tasks that can be distributed in the map-reduce framework.

--- passCrack.py
+++ passCrack.py
@@ -12,39 +12,86 @@ import hashlib
     
 
 #Generate a list of numbers from 2 to 1 million.
-data = range(2, 10000000)
-data = map(lambda item: str(item), data)
+hasher = hashlib.md5()
+#hasher.update('string')
+#hasher.hexdigest()[0:5]
+#97 - 122, 48 - 57
+passwordHash = 'd077f'
+
+data = {num: password for num in range(0, 4)}
+
 
 #The data must be enumerated in a dictionary, with integer keys so that the 
 #mincemeat code works. This is bc mincemeat expects inputs with an integer 
 #as key.
 datasource = dict(enumerate(data))
 
-
-def mapfn(k, v):    
+def mapfn(k, v):
     '''
     Determine if a number is prime, and yield it if true.
     '''    
-    #Yield an amount for each key.
-    for value in v:
-        yield value, 1
-
-
+    charArray = range(97, 123)
+    charArray2 = range(48, 58)
+    charArray.extend(charArray2)    
+    
+    hasher = hashlib.md5()  
+    
+    
+    #For each character position, generate a new password hash.
+    for i0 in range(0, 1):
+        pass
+        
+        
+    #After all permutations have been generated, add the hash of the password and the password to the list.
+    #If an array has an item, the reduce it.
+    yield v, 1
+    
+        
+    
+    
 def reducefn(k, vs):
+        
+    
     return vs
+    
+
+def genComb(rootStrList = "", maxSize = 4):
+    #TODO: Add code to initialize the stack and run dfs for each root str.    
+    stack = expandItem(rootStrList)
+    while len(stack) > 0:
+        item = stack.pop()
+        print(item)
+        if len(item) < maxSize:
+            for st in expandItem(item):
+                stack.append(st)
+
+
+def expandItem(item):
+    charArray = range(97, 123)
+    charArray2 = range(48, 58)
+    charArray.extend(charArray2)
+    charArray = map(lambda item: chr(item), charArray)
+    retArray = [] 
+    for char in charArray:
+        retArray.append(char + item)
+    
+    return retArray
+
+    
+
 
 #Set up the "Server", which is really the code to run on the mapreduce workers or "Client".
-s = mincemeat.Server()
-s.datasource = datasource
-s.mapfn = mapfn
-s.reducefn = reducefn
-
-# Run the mapreduce job, and store the result in "results".
-startTime = time.time()
-#Generate all numbers from 2 to 10,000,000 that are prime palindromes.
-results = s.run_server(password="changeme")
-endTime = time.time()
-fullTime = (endTime - startTime) / 60.0
-print("Time to run code: " + str(fullTime))
-print("Results: ")
-print(str(results))
+#s = mincemeat.Server()
+#s.datasource = datasource
+#s.mapfn = mapfn
+#s.reducefn = reducefn
+#
+## Run the mapreduce job, and store the result in "results".
+#startTime = time.time()
+##Generate all numbers from 2 to 10,000,000 that are prime palindromes.
+#results = s.run_server(password="changeme")
+#endTime = time.time()
+#fullTime = (endTime - startTime) / 60.0
+#print("Time to run code: " + str(fullTime))
+#print("Results: ")
+#print(str(results))
